c49ea36462a9a118dab1196540fd82de7c1923d5
Anthony Ruffino <aruffino84@gmail.com>
2020-10-03 05:25:59 +0200

    More windows support

--- buildWindows.sh
+++ buildWindows.sh
@@ -1,3 +1,4 @@
+#wget -O ~/Downloads/OpenJDK11U-jre_x64_windows_openj9_11.0.8_10_openj9-0.21.0.zip https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10_openj9-0.21.0/OpenJDK11U-jre_x64_windows_openj9_11.0.8_10_openj9-0.21.0.zip
 java -jar packr-all-2.7.0.jar  \
 	--platform windows64   \
      	--jdk ~/Downloads/OpenJDK11U-jre_x64_windows_openj9_11.0.8_10_openj9-0.21.0.zip  \
