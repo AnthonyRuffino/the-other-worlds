db8063b0728df4430c87fb26232c109466e0807a
Anthony Ruffino <aruffino84@gmail.com>
2020-11-18 19:21:50 +0100

    run file

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 ./gradlew :plugin:build
-./gradlew :plugin:build
+./gradlew :core:build
 ./gradlew :example:build
+./gradlew :desktop:build
 mkdir -p desktop/plugins/example
 cp example/build/libs/example-1.0.jar desktop/plugins/example/example.jar
 ./gradlew :desktop:run
